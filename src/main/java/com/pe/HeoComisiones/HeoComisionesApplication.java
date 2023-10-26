334f1d846fe142a1f6e9756285f7f46c962860ff
Giuseppe0311 <barrerita_romero@hotmail.com>
2023-10-26 12:35:24 -0500

    first commit

---
+++ src/main/java/com/pe/HeoComisiones/HeoComisionesApplication.java
@@ -0,0 +1,13 @@
+package com.pe.HeoComisiones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeoComisionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeoComisionesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pe/HeoComisiones/HeoComisionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pe.HeoComisiones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeoComisionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
