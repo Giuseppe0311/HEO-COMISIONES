68c28e0b9be4786065cead9fe43de7ddbe929ef1
Giuseppe0311 <barrerita_romero@hotmail.com>
2023-11-20 16:35:44 -0500

    SE CORRIGIO EL ERROR DE MUESTRA DE USUARIOS SIN COMISIONES ASIGNADA

--- src/main/java/com/pe/HeoComisiones/Repository/UsuarioRepository.java
+++ src/main/java/com/pe/HeoComisiones/Repository/UsuarioRepository.java
@@ -11,7 +11,10 @@ public interface UsuarioRepository extends JpaRepository<Usuarios, Integer> {
     Optional<Usuarios> findByUsername(String username);
     @Query("SELECT u FROM Usuarios u WHERE u.status = true AND u.dni NOT IN ('41846665', '01116630', '40070789')")
     List<Usuarios> findByStatusTrueAndDniNotIn();
-    @Query("SELECT u FROM Usuarios u LEFT JOIN Comisiones c ON u.id = c.id_usuario " +
-            "WHERE u.status = true AND c.id_usuario IS NULL")
+    @Query(value = "SELECT u.* " +
+            "FROM Usuarios u " +
+            "LEFT JOIN Comisiones c ON u.id = c.id_usuario " +
+            "WHERE u.status = true AND c.id_usuario IS NULL AND u.dni NOT IN ('01116630', '41846665', '40070789')",
+            nativeQuery = true)
     List<Usuarios> findUsuariosSinComisiones();
 }
