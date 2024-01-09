93fd18537964c8350f6f0a48281b6dd76e9b3dc5
Giuseppe0311 <barrerita_romero@hotmail.com>
2024-01-09 12:11:08 -0500

    MANEJO DE CONTRATOS Y TAREAS AUTOMATIZADAS

--- src/main/java/com/pe/HeoComisiones/Repository/UsuarioRepository.java
+++ src/main/java/com/pe/HeoComisiones/Repository/UsuarioRepository.java
@@ -3,10 +3,12 @@ package com.pe.HeoComisiones.Repository;
 import com.pe.HeoComisiones.Entity.Usuarios;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface UsuarioRepository extends JpaRepository<Usuarios, Integer> {
     Optional<Usuarios> findByUsername(String username);
     @Query("SELECT u FROM Usuarios u WHERE u.status = true AND u.dni NOT IN ('41846665', '01116630', '40070789')")
--- src/main/java/com/pe/HeoComisiones/Services/UserDetailsLoaderService.java
+++ src/main/java/com/pe/HeoComisiones/Services/UserDetailsLoaderService.java
@@ -1,6 +1,7 @@
 package com.pe.HeoComisiones.Services;
 
 import com.pe.HeoComisiones.Repository.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -8,9 +9,10 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserDetailsLoaderService implements UserDetailsService {
-    @Autowired
-    private UsuarioRepository usuarioRepository;
+
+    private final UsuarioRepository usuarioRepository;
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return usuarioRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("user is not valid"));
